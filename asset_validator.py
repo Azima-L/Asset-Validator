cc9f803e8528131e0f0510c8ee43ec1df111af22
Azima Leyyan <azimaleyyan4@gmail.com>
2026-08-19 16:56:25 +0300

    v0.1 - Initial version
    Started an Asset Validator project where I am going to build from scratch.
    
    Learning PySide6 for a GUI for the first time.

---
+++ asset_validator.py
@@ -0,0 +1,56 @@
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+import sys
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Asset Validator")
+        self.setFixedSize(400, 200)
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.label = QLabel("Press Run to validate assets")
+        layout.addWidget(self.label)
+
+        button = QPushButton("Run Validator")
+        button.clicked.connect(self.run_validator)
+        layout.addWidget(button)
+
+    def is_valid_asset_name(self, file_name):
+        if not file_name.startswith(("SM_", "SK_", "T_")):
+            return False
+        if not file_name.endswith((".fbx", ".png")):
+            return False
+        if len(file_name) < 9:
+            return False
+        return True
+
+    def run_validator(self):
+        assets = [
+            "SM_Rock.fbx",
+            "rock.fbx",
+            "T_.png",
+            "SK_Hero.png",
+            "SM_Tree.obj"
+            ]
+
+        valid_counter = 0
+        invalid_counter = 0
+        failed_files = []
+
+        for asset in assets:
+            if self.is_valid_asset_name(asset):
+                valid_counter += 1
+            else:
+                failed_files.append(asset)
+                invalid_counter += 1
+
+        self.label.setText(f"{valid_counter} valid, {invalid_counter} invalid:\n"
+                           f"{", ".join(failed_files)}")
+    
+
+app = QApplication(sys.argv)
+window = MyWindow()
+window.show()
+app.exec()
